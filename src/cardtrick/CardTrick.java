9b408bb121c60c904affd8e3a1744afed0f9c0cf
Sanket <Sanket@192.168.2.54>
2021-02-10 20:43:23 -0500

    First commit. Before any changes

---
+++ src/cardtrick/CardTrick.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cardtrick;
+
+/**
+ *
+ * @author Sanket
+ */
+public class CardTrick {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         Card[] magicHand = new Card[7];
+        
+        for (int i=0; i<magicHand.length; i++)
+        {
+            Card c = new Card();
+            //c.setValue(insert call to random number generator here)
+            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+        }
+        
+        //insert code to ask the user for Card value and suit, create their card
+        // and search magicHand here
+        //Then report the result here
+        
+    }
+    
+}
